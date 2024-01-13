e49d7e63db9f1cfd8c8427faa7f1127e83db076e
rubishaa <96764817+rubishaa@users.noreply.github.com>
2024-01-13 14:12:37 +1100

    Update config.py

--- OrderService/app/config.py
+++ OrderService/app/config.py
@@ -1,5 +1,5 @@
-NGROK_AUTH_TOKEN = '2WjiRTcdEhd5EOZZvVi85tiu3rs_5ZT17RCy2dotiNpmUq2Ew'
+NGROK_AUTH_TOKEN = 'abcd
 PRODUCT_URL = 'http://localhost:8081/products'
 USER_URL = 'http://localhost:8082/users'
-DB_URL = "postgresql://anet:vUdRee4BMHdbBaavGKEPefx9lwktPPO8@dpg-ckkv6f3j89us73bdu3q0-a.oregon-postgres.render.com/order_service"
-MQ_URL = 'amqps://csmzdbia:5IGG1qeiX6ACePGZ8223WDt4SRn1lu11@albatross.rmq.cloudamqp.com/csmzdbia'
+DB_URL = "postgresql:/name:aaaaaaaa-a.oregon-postgres.render.com/order_service"
+MQ_URL = 'amqps://aaaaaaaaaaa1@albatross.rmq.cloudamqp.com/csmzdbia'
